15e036ea76912ccf3a967b21aa0ca11a44c08ac0
Cleeph <mboyacliffoch@gmail.com>
2021-08-31 14:01:55 -0700

    Task _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
- * main - print Holberton
+ * main - print _putchar
+ *
  * Return: Always 0
+ *
  */
 int main(void)
 {
-char c[9] = "_putchar";
+char *c = "_putchar";
 int i;
-for (i = 0; i < 10; i++)
-  _putchar(c[i]);
- _putchar('\n');
-   return (0);
+for (i = 0; i < 10; i++);
+_putchar(c[i]);
+
+_putchar('\n');
+return (0);
 }
