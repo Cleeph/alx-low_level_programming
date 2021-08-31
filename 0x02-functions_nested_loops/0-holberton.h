fd9e053d0ae558a22ee1d6890816ab0bc501369a
Cleeph <mboyacliffoch@gmail.com>
2021-08-31 01:28:42 -0700

    Task _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.h
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/* Print Holberton
+ */
+
+int main(void)
+  
+{
+  
+  char *sh = "Holberton";
+  
+
+  
+  while (*sh)
+    
+    {
+      
+      _putchar(*sh);
+      
+      sh++;
+      
+    }
+  
+  _putchar('\n');
+  
+
+  
+  return (0);
+  
+}
