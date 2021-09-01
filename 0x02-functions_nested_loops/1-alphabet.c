a9e9b672e47d31f5ddcc056cf906d202965fd200
Cleeph <mboyacliffoch@gmail.com>
2021-09-01 08:37:59 -0700

    Print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,11 @@
  *
  * Return: Always 0.
  */
-int main(void)
-
+void print_alphabet(void)
 {
-char print_alphabet(void);
-char n;
-for (n = 'a'; n <= 'z'; n++)
+  
+  char n;
+  for (n = 'a'; n <= 'z'; n++)
 _putchar(n);
 _putchar('\n');
-return (0);
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,6 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar (char c);
-
-#endif
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
