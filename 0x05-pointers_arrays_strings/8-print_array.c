487ed5475bc8e56128b419bed17311ec59aa4fa1
Cleeph <mboyacliffoch@gmail.com>
2021-09-07 13:07:55 -0700

    Task Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,25 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - check the code for Holberton School students.
- * @a: pointer int type
- * @n: is a int variable
- * Return: Always 0.
+ * print_array - Prints an inputted number of elements
+ *               of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
  */
 void print_array(int *a, int n)
 {
-int i, inte;
+	int index;
 
-for (i = 0; i < n; i++)
-{
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
 
-inte = *(a + i);
+		if (index == n - 1)
+			continue;
 
-printf("%d", inte);
+		printf(", ");
+	}
 
-if (i != n - 1)
-printf(", ");
-}
-
-printf("\n");
+	printf("\n");
 }
