d16686e142710b387feed84d65f1f1416f46c070
Cleeph <mboyacliffoch@gmail.com>
2021-09-14 04:40:54 -0700

    Advanced Task Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - locates character in string
+ * @s: pointer of a pointer type char
+ * @to: pointer type char
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
